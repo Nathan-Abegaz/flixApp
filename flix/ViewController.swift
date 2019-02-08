29b4153bb771f196996463718b9f69579fb64d93
Nathan <abegaznathan@gmail.com>
2019-02-07 20:14:01 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Nathan  Abegaz on 2/1/19.
+//  Copyright © 2019 Nathan  Abegaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
